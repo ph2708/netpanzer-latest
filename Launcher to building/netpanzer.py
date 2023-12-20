8da6b0de2c1a89b5f26e60b525bff4aa6ae8a564
Phelipe Silva Costa <56886364+ph2708@users.noreply.github.com>
2023-12-19 23:11:25 -0300

    .

--- Launcher to building/netpanzer.py
+++ Launcher to building/netpanzer.py
@@ -4,12 +4,10 @@ import zipfile
 import subprocess
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
 import win32com.client
-import json
 
 def get_current_version(install_dir):
-    current_version = "1.0.0"  # Versão padrão se o arquivo não existir
+    current_version = "1.0.0"  # Default version if the file does not exist
     try:
         with open(os.path.join(install_dir, "current_version.txt"), "r") as file:
             current_version = file.read().strip()
@@ -20,6 +18,19 @@ def get_current_version(install_dir):
 def set_current_version(install_dir, version):
     with open(os.path.join(install_dir, "current_version.txt"), "w") as file:
         file.write(version)
+
+def get_latest_version_from_json(json_url):
+    try:
+        response = requests.get(json_url)
+        data = response.json()
+        latest_version = data.get("version")
+        if latest_version:
+            return latest_version
+        else:
+            raise ValueError("Could not retrieve the latest version from the JSON file.")
+    except Exception as e:
+        print(f"Error in obtaining the latest version: {e}")
+        return None
 
 def create_desktop_shortcut(install_dir, target_path):
     desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
@@ -44,81 +55,82 @@ def get_download_url_from_json(json_url):
 
 def install_game(install_dir, progress_label, json_url):
     try:
-        progress_label.config(text="Baixando e instalando o jogo...")
+        progress_label.config(text="Downloading and installing the game...")
         progress_label.update()
 
-        # Certifique-se de ter permissões administrativas para escrever na pasta de instalação
         os.makedirs(install_dir, exist_ok=True)
 
-        # Obter o link de download do JSON
         download_url = get_download_url_from_json(json_url)
-
-        # Baixar o arquivo ZIP da versão mais recente
         zip_path = os.path.join(install_dir, os.path.basename(download_url))
         download_file(download_url, zip_path)
 
-        # Extrair o conteúdo do arquivo ZIP para a pasta de instalação
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall(install_dir)
 
-        # Remover o arquivo ZIP após a instalação
         os.remove(zip_path)
 
-        # Atualizar a versão atual após a instalação
-        latest_version = get_current_version(install_dir)
-        set_current_version(install_dir, latest_version)
+        latest_version = get_latest_version_from_json(json_url)
+        if latest_version:
+            set_current_version(install_dir, latest_version)
+        else:
+            raise Exception("Could not retrieve the latest version of the game.")
 
-        progress_label.config(text="Instalação concluída.")
+        progress_label.config(text="Installation complete.")
         progress_label.update()
+
+        launch_game(install_dir)
 
     except Exception as e:
-        progress_label.config(text=f"Ocorreu um erro durante a instalação: {str(e)}")
+        progress_label.config(text=f"An error occurred during installation: {str(e)}")
         progress_label.update()
+    finally:
+        close_window()
+
+
 
 def check_version(install_dir, progress_label, json_url):
     try:
-        progress_label.config(text="Verificando a versão do jogo...")
+        progress_label.config(text="Checking the game version...")
         progress_label.update()
 
-        # Obter a versão atual do jogo
         current_version = get_current_version(install_dir)
+        latest_version = get_latest_version_from_json(json_url)
 
-        # Verificar se o arquivo executável do jogo existe
         game_exe_path = os.path.join(install_dir, "netpanzer.exe")
 
-        if not os.path.exists(game_exe_path):
-            progress_label.config(text="Jogo não encontrado. Realizando a instalação...")
+        if not os.path.exists(game_exe_path) or current_version != latest_version:
+            progress_label.config(text="Game not found or outdated. Performing installation...")
             progress_label.update()
-
-            # Executar a instalação completa
             install_game(install_dir, progress_label, json_url)
         else:
-            progress_label.config(text="Seu jogo está instalado e atualizado.")
-
-        # Criar o atalho para o launcher.exe com o nome "netpanzer"
-        launcher_path = os.path.join(install_dir, "launcher.exe")
-
-        # Criar o atalho na área de trabalho
-        create_desktop_shortcut(install_dir, launcher_path)
-
-        # Executar o jogo após a verificação e instalação
-        subprocess.Popen([game_exe_path], cwd=install_dir)
-
-        # Fechar a janela do tkinter após a execução do jogo
-        root.destroy()
+            progress_label.config(text="Your game is installed and up to date.")
+            progress_label.update()
+            launch_game(install_dir)
 
     except Exception as e:
-        progress_label.config(text=f"Ocorreu um erro inesperado: {str(e)}")
+        progress_label.config(text=f"An unexpected error occurred: {str(e)}")
+    finally:
+        close_window()
+
+def launch_game(install_dir):
+    game_exe_path = os.path.join(install_dir, "netpanzer.exe")
+    if os.path.exists(game_exe_path):
+        subprocess.Popen([game_exe_path], cwd=install_dir)
+    else:
+        print("Game executable not found.")
+
+def close_window():
+    root.after(1000, root.destroy)  # Wait for 1 second before closing the window
 
 if __name__ == "__main__":
-    install_dir = 'C:\\Netpanzer'  # Defina a pasta de instalação
-    json_url = 'https://github.com/ph2708/netpanzer-latest/raw/main/version.json'  # URL do arquivo JSON com o link de download
+    install_dir = 'C:\\Netpanzer'
+    json_url = 'https://github.com/ph2708/netpanzer-latest/raw/main/version.json'
 
     root = tk.Tk()
-    root.title("Verificando Atualizações")
+    root.title("Checking Updates")
     root.geometry("300x100")
 
-    progress_label = ttk.Label(root, text="Verificando atualizações...")
+    progress_label = ttk.Label(root, text="Checking updates...")
     progress_label.pack(pady=20)
 
     check_version(install_dir, progress_label, json_url)
